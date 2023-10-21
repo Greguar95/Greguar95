5aeca35b109127b866800f068cba133e19c0a052
Greg <qosound@yahoo.com>
2023-10-21 13:52:58 -0500

    exercise941

---
+++ exercise94.py
@@ -0,0 +1,20 @@
+fname = input('Enter file:')
+try:
+    fhand = open(fname)
+except:
+    fname = "mbox-short.txt"
+    fhand = open(fname)
+dc = dict()
+for line in fhand:
+    if line.startswith('From '):
+        line = line.rstrip()
+        words = line.split()
+        eml = words[1]
+        dc[eml] = dc.get(eml, 0) + 1
+maxeml = None
+maxcnt = None
+for eml, cnt in dc.items():
+    if maxcnt is None or cnt > maxcnt:
+        maxeml = eml
+        maxcnt = cnt
+print(maxeml, maxcnt)
